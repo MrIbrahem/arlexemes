1c11ffe5208657112c85b155aad8fe8d23253822
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-07-25 04:17:48 +0300

    u

--- public_html/index.php
+++ public_html/index.php
@@ -56,24 +56,32 @@ require __DIR__ . "/main.php";
                     <div class="row">
                         <div class="col-md-5">
                             <span class="me-2">اسم:</span><br>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L1244618')">L1244618</button>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L2465')">L2465 -
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1473670')">L1473670</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setExample('L2465')">L2465 -
                                 معدود</button>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L2355')">L2355 - غير
+                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setExample('L2355')">L2355 - غير
                                 معدود</button>
                         </div>
                         <div class="col-md-4">
                             <span class="me-2">صفة:</span><br>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L1131459')">L1131459</button>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L1153733')">L1153733</button>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L1153733')">L1153733</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1131459')">L1131459</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1473674')">L1473674</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1472818')">L1472818</button>
                         </div>
                         <div class="col-md-3">
                             <span class="me-2">فعل:</span><br>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L1460479')">L1460479</button>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L12100')">L12100</button>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L1235177')">L1235177</button>
-                            <button class="btn btn-outline-secondary btn-sm me-1" onclick="setLabel('L1464180')">L1464180</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1474373')">L1474373</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1474244')">L1474244</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1473584')">L1473584</button>
+                            <button class="btn btn-outline-secondary btn-sm me-1"
+                                onclick="setExample('L1474044')">L1474044</button>
                         </div>
                     </div>
                 </div>
