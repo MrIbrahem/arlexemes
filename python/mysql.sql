af6a2e4624a10c17189f3dac9ad2d6ddb3f281d8
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-08-12 05:24:46 +0300

    z

--- python/mysql.sql
+++ python/mysql.sql
@@ -10,7 +10,7 @@ SET NAMES utf8mb4;
 DROP TABLE IF EXISTS `lemmas_p11038`;
 CREATE TABLE `lemmas_p11038` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `lemma_id` int NOT NULL,
+  `lemma_id` bigint NOT NULL,
   `lemma` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   `pos` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '',
   `pos_cat` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT '',
@@ -22,11 +22,6 @@ CREATE TABLE `lemmas_p11038` (
   KEY `lemma_id` (`lemma_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
-
-DROP VIEW IF EXISTS `p11038_lemmas_match`;
-CREATE TABLE `p11038_lemmas_match` (`id` int);
-
-
 DROP TABLE IF EXISTS `wd_data`;
 CREATE TABLE `wd_data` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -35,24 +30,28 @@ CREATE TABLE `wd_data` (
   `lemma` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `wd_id2` (`wd_id`),
-  KEY `wd_id` (`wd_id`)
+  KEY `wd_id` (`wd_id`),
+  KEY `lemma` (`lemma`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
-
-DROP VIEW IF EXISTS `wd_data_both`;
-CREATE TABLE `wd_data_both` (`vi_wd_id` varchar(255), `vi_wd_id_category` varchar(255), `vi_lemma` varchar(255), `vi_value` varchar(255));
-
 
 DROP TABLE IF EXISTS `wd_data_p11038`;
 CREATE TABLE `wd_data_p11038` (
   `id` int NOT NULL AUTO_INCREMENT,
   `wd_data_id` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `value` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `value` bigint NOT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `wd_data_id` (`wd_data_id`,`value`),
-  KEY `wd_data_id_value` (`wd_data_id`,`value`),
+  UNIQUE KEY `wd_data_id_value` (`wd_data_id`,`value`),
+  KEY `wd_data_id` (`wd_data_id`),
+  KEY `value` (`value`),
   CONSTRAINT `fk_wd_data` FOREIGN KEY (`wd_data_id`) REFERENCES `wd_data` (`wd_id`) ON DELETE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+DROP VIEW IF EXISTS `p11038_lemmas_match`;
+CREATE TABLE `p11038_lemmas_match` (`id` int);
+
+
+DROP VIEW IF EXISTS `wd_data_both`;
+CREATE TABLE `wd_data_both` (`vi_wd_id` varchar(255), `vi_wd_id_category` varchar(255), `vi_lemma` varchar(255), `vi_value` bigint);
 
 
 DROP VIEW IF EXISTS `wd_data_view`;
@@ -68,8 +67,4 @@ CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `wd_data_both` AS select `d
 DROP TABLE IF EXISTS `wd_data_view`;
 CREATE ALGORITHM=UNDEFINED SQL SECURITY DEFINER VIEW `wd_data_view` AS select `d`.`wd_id` AS `wd_id`,`d`.`wd_id_category` AS `wd_id_category`,`d`.`lemma` AS `lemma`,group_concat(`p`.`value` separator ', ') AS `P11038_values` from (`wd_data` `d` left join `wd_data_p11038` `p` on((`d`.`wd_id` = `p`.`wd_data_id`))) group by `d`.`wd_id`,`d`.`wd_id_category`,`d`.`lemma`;
 
--- 2025-08-04 21:05:29 UTC
-Cc
-W
-.*
-
+-- 2025-08-12 02:10:00 UTC
---
+++ python/src/pyx/wd_data_bots/wd_data_P11038_new.py
@@ -0,0 +1,200 @@
+# -*- coding: utf-8 -*-
+"""
+
+from wd_data_bots import wd_data_P11038
+# all_result, db_exec_time = wd_data_P11038.get_lemmas(limit=limit, offset=offset, order=order, order_by=order_by, filter_data=filter_data)
+# counts, db_exec_time = wd_data_P11038.count_all_p11038()
+
+"""
+from ..logs_db.db_mysql import fetch_all
+
+
+def add_order_limit_offset(query, params, order_by, order, limit, offset):
+    # ---
+    if order not in ["ASC", "DESC"]:
+        order = "DESC"
+    # ---
+    if order_by:
+        query += f" ORDER BY {order_by} {order}"
+    # ---
+    if limit > 0:
+        query += " LIMIT %s"
+        params.extend([limit])
+    # ---
+    if offset > 0:
+        query += " OFFSET %s"
+        params.extend([offset])
+    # ---
+    return query, params
+
+
+def get_lemmas_with(limit=0, offset=0, order="DESC", order_by="id"):
+    # Fast
+    # ---
+    query = """
+        SELECT
+            l.id, l.lemma_id, l.lemma, l.pos, l.pos_cat, l.sama_lemma_id, l.sama_lemma,
+            w.wd_id as vi_wd_id, w.wd_id_category as vi_wd_id_category, w.lemma as vi_lemma, wdp.value as vi_value
+
+        FROM lemmas_p11038 AS l
+            JOIN wd_data_p11038 AS wdp
+        ON l.lemma_id = wdp.value
+            JOIN wd_data AS w ON wdp.wd_data_id = w.wd_id
+            UNION ALL
+
+        SELECT
+            l.id, l.lemma_id, l.lemma, l.pos, l.pos_cat, l.sama_lemma_id, l.sama_lemma,
+            w.wd_id as vi_wd_id, w.wd_id_category as vi_wd_id_category, w.lemma as vi_lemma, wdp.value as vi_value
+        FROM lemmas_p11038 AS l
+            JOIN wd_data_p11038 AS wdp
+        ON l.sama_lemma_id = wdp.value
+            JOIN wd_data AS w ON wdp.wd_data_id = w.wd_id
+    """
+    # ---
+    params = []
+    # ---
+    query, params = add_order_limit_offset(query, params, order_by, order, limit, offset)
+    # ---
+    logs, db_exec_time = fetch_all(query, params)
+    # ---
+    return logs, db_exec_time
+
+
+def get_lemmas_without(limit=0, offset=0, order="DESC", order_by="id"):
+    # Fast
+    # ---
+    query_lemmas_without_slow = """
+        SELECT
+            l.id, l.lemma_id, l.lemma, l.pos, l.pos_cat, l.sama_lemma_id, l.sama_lemma
+        FROM
+            lemmas_p11038 AS l
+            LEFT JOIN wd_data_p11038 AS wdp ON l.lemma_id = wdp.value
+            LEFT JOIN wd_data_p11038 AS wdp2 ON l.sama_lemma_id = wdp2.value
+        WHERE
+            wdp.value IS NULL AND wdp2.value IS NULL
+        LIMIT 200
+        OFFSET 50000
+    """
+    # ---
+    query = """
+        SELECT
+            l.id, l.lemma_id, l.lemma, l.pos, l.pos_cat, l.sama_lemma_id, l.sama_lemma
+        FROM
+            lemmas_p11038 AS l
+        WHERE
+            l.id NOT IN (
+                SELECT l2.id
+                FROM lemmas_p11038 AS l2
+                    JOIN wd_data_p11038 AS wdp2
+                    ON l2.lemma_id = wdp2.value
+                    #JOIN wd_data AS w ON wdp2.wd_data_id = w.wd_id
+            )
+        AND
+            l.id NOT IN (
+                SELECT l2.id
+                FROM lemmas_p11038 AS l2
+                    JOIN wd_data_p11038 AS wdp2
+                    ON l2.sama_lemma_id = wdp2.value
+                    #JOIN wd_data AS w ON wdp2.wd_data_id = w.wd_id
+            )
+    """
+    # ---
+    query = """
+        SELECT
+            l.id, l.lemma_id, l.lemma, l.pos, l.pos_cat, l.sama_lemma_id, l.sama_lemma
+        FROM
+            lemmas_p11038 AS l
+        WHERE
+            l.lemma_id NOT IN ( SELECT value FROM wd_data_p11038 )
+        AND
+            l.sama_lemma_id NOT IN ( SELECT value FROM wd_data_p11038 )
+    """
+    # ---
+    params = []
+    # ---
+    query, params = add_order_limit_offset(query, params, order_by, order, limit, offset)
+    # ---
+    logs, db_exec_time = fetch_all(query, params)
+    # ---
+    return logs, db_exec_time
+
+
+def count_lemmas_p11038():
+    # Fast
+    # ---
+    query = "SELECT COUNT(*) AS total_rows FROM lemmas_p11038"
+    # ---
+    result, db_exec_time = fetch_all(query, [], fetch_one=True)
+    # ---
+    if not result:
+        return {}, db_exec_time
+    # ---
+    if isinstance(result, list):
+        result = result[0]
+    # ---
+    total_rows = int(result["total_rows"])
+    # ---
+    return total_rows, db_exec_time
+
+
+def count_has_values():
+    # FAST
+    # ---
+    query = """
+        SELECT
+            COUNT(value) AS count_has_value
+        FROM (
+            -- استعلام للربط على lemma_id
+            SELECT wdp.value FROM lemmas_p11038 AS l
+                JOIN wd_data_p11038 AS wdp ON l.lemma_id = wdp.value
+
+            UNION ALL
+            -- استعلام للربط على sama_lemma_id
+            SELECT wdp.value FROM lemmas_p11038 AS l
+                JOIN wd_data_p11038 AS wdp ON l.sama_lemma_id = wdp.value
+        ) AS combined_results
+    """
+    # ---
+    result, db_exec_time = fetch_all(query, [], fetch_one=True)
+    # ---
+    if not result:
+        return {}, db_exec_time
+    # ---
+    if isinstance(result, list):
+        result = result[0]
+    # ---
+    has_value = int(result["count_has_value"])
+    # ---
+    return has_value, db_exec_time
+
+
+def count_all_p11038():
+    # ---
+    total_rows, db_exec_time_1 = count_lemmas_p11038()
+    # ---
+    has_value, db_exec_time_2 = count_has_values()
+    # ---
+    db_exec_time = db_exec_time_1 + db_exec_time_2
+    # ---
+    total_rows = total_rows // 2
+    # ---
+    data = {
+        "all": total_rows,
+        "with": has_value,
+        "without": total_rows - has_value,
+    }
+    # ---
+    return data, db_exec_time
+
+
+def get_lemmas(limit=0, offset=0, order="DESC", order_by="id", filter_data="with"):
+    # ---
+    logs = []
+    db_exec_time = 0
+    # ---
+    if filter_data == "without":
+        logs, db_exec_time = get_lemmas_without(limit, offset, order, order_by)
+    else:
+        logs, db_exec_time = get_lemmas_with(limit, offset, order, order_by)
+    # ---
+    return logs, db_exec_time
