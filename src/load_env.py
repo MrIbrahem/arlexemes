d32f3eea9aad876937063b410f6372c4d91673ec
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-15 03:11:15 +0300

    .

---
+++ src/load_env.py
@@ -0,0 +1,8 @@
+
+from pathlib import Path
+from dotenv import load_dotenv
+
+_env_file_path = Path(__file__).parent / ".env"
+
+if _env_file_path.exists():
+    load_dotenv(str(_env_file_path))
--- src/main_app/logs_db/config_db.py
+++ src/main_app/logs_db/config_db.py
@@ -1,33 +1,19 @@
-from configparser import ConfigParser
-from pathlib import Path
+import os
 
 from pymysql.cursors import DictCursor
 
 
-def load_db_config():
-    # --- 1) تحقق من ملف الإنتاج ~/replica.my.cnf ---
-    replica_cnf_path = Path.home() / "replica.my.cnf"
-    if replica_cnf_path.exists():
-        parser = ConfigParser()
-        parser.read(replica_cnf_path)
-
-        user = parser.get("client", "user", fallback=None)
-        password = parser.get("client", "password", fallback=None)
-
-        return {
-            "host": "tools.db.svc.wikimedia.cloud",
-            "user": user,
-            "password": password,
-            "database": f"{user}__arlexemes",
-            "charset": "utf8mb4",
-            "cursorclass": DictCursor,
-        }
+def load_db_config():  # -> dict[str, Any]:
+    TOOL_REPLICA_PASSWORD = os.environ.get("TOOL_REPLICA_PASSWORD", "root11")
+    TOOL_REPLICA_USER = os.environ.get("TOOL_REPLICA_USER", "root")
+    DB_HOST_TOOLS = os.environ.get("DB_HOST_TOOLS", "localhost")
+    TOOL_DB_NAME = os.environ.get("TOOL_DB_NAME", "arlexemes")
 
     return {
-        "host": "localhost",
-        "user": "root",
-        "password": "root11",
-        "database": "arlexemes",
+        "host": DB_HOST_TOOLS,
+        "user": TOOL_REPLICA_USER,
+        "password": TOOL_REPLICA_PASSWORD,
+        "database": TOOL_DB_NAME,
         "charset": "utf8mb4",
         "cursorclass": DictCursor,
     }
